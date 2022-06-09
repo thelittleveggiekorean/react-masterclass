63c49a34476d85e960dee57eab6dae27cb91a5e9
Chohee <chohee4421@gmail.com>
2022-06-09 20:09:51 +0900

    Add google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import {createGlobalStyle} from 'styled-components';
 
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap');
-
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
